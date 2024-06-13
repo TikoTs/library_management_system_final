f924e025e774b4b1898cfb601f167947334a4a39
TikoTs <tiko.tsakadze2018@gmail.com>
2024-06-13 04:47:59 +0400

    Push __init__ for views

---
+++ library_app/views/__init__.py
@@ -0,0 +1,29 @@
+from library_app.views.views import (
+    AuthorViewSet,
+    GenreViewSet,
+    BookListView,
+    BookDetailView,
+    BookCreateAPIView,
+    BookUpdateAPIView,
+    AllBooksListView,
+    AvailableBookTitlesView,
+    BookReservationCreateView,
+    BooksBorrowUpdateView,
+    BookRequestCreateView,
+)
+from library_app.views.statistics_views import StatisticsAPIView
+
+__all__ = [
+    "AuthorViewSet",
+    "GenreViewSet",
+    "BookListView",
+    "BookDetailView",
+    "BookCreateAPIView",
+    "BookUpdateAPIView",
+    "AllBooksListView",
+    "AvailableBookTitlesView",
+    "BookReservationCreateView",
+    "BooksBorrowUpdateView",
+    "BookRequestCreateView",
+    "StatisticsAPIView",
+]
